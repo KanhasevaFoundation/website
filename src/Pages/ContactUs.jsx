96388bf860d6c71d8a4ce10ff3c5b9e9deaa9da5
KanhasevaFoundation <kanhaseva.in@gmail.com>
2025-03-23 17:46:48 +0530

    Update ContactUs.jsx

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -25,7 +25,7 @@ const ContactUs = () => {
     setSuccessMessage("");
   
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://kanhaseva-in.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
