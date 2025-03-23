8387e8e816d22d90c1c8f5d477544d285ef7dafe
KanhasevaFoundation <kanhaseva.in@gmail.com>
2025-03-23 17:47:12 +0530

    Update DonorsPage.jsx

--- src/Pages/DonorsPage.jsx
+++ src/Pages/DonorsPage.jsx
@@ -8,7 +8,7 @@ const AdminDonorListPage = () => {
   useEffect(() => {
     // Fetch donations from the server
     axios
-      .get("http://localhost:5000/api/donations")
+      .get("https://kanhaseva-in.onrender.com/api/donations")
       .then((response) => setDonations(response.data))
       .catch((error) => console.error("Error fetching donations:", error));
   }, []);
